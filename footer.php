97d66063ecd0c6bb7dd4a13d24f0107710df19e3
jayvardhan <vardhan.jay@gmail.com>
2021-07-22 16:33:07 +0530

    updated footer logo

--- footer.php
+++ footer.php
@@ -5,7 +5,12 @@
         <footer>
             <div class="container">
                 <div class="row">
-                    <div class="col-sm-12"> <?php 
+                    <div class="col-sm-12">
+                        <?php $logo = BTP_DIR_URI . '/assets/images/logo-footer.png'; ?> 
+                        <a href="<?php _e(home_url());?>">
+                            <img src="<?php _e($logo);?>" alt="site-logo-footer" class="site-logo-footer">
+                        </a>
+                        <?php 
                         if ( is_active_sidebar( 'footer-sidebar' ) ) {
                             dynamic_sidebar( 'footer-sidebar' );
                         } ?>
