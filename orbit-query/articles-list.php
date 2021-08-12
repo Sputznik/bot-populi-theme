1c5792751c7cadc67069c5e98e1213425c080beb
jayvardhan <vardhan.jay@gmail.com>
2021-08-12 17:12:34 +0530

    fixed tag template styles

--- orbit-query/articles-list.php
+++ orbit-query/articles-list.php
@@ -6,10 +6,9 @@ add_filter( 'excerpt_length', function($length) { return 26; }, 999 );
 <div id="<?php _e( $atts['id'] );?>" class="orbit-post-grid" data-target="article.post-list" data-url="<?php _e( $atts['url'] );?>">
 	<?php while( $this->query->have_posts() ) : $this->query->the_post();?>
         <?php $post_type = get_post_type();?>
-        <article class='post-list <?php _e($post_type);?>'>
+        <article class='post-list <?php _e($post_type);?>'> 
             <?php get_template_part( 'template-parts/orbit/list' );?>
         </article>
-        <div class="page-title-separator"></div>
 	<?php endwhile;?>
 </div>
 
