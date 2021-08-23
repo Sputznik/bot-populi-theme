7afe34b46eecca00f191611fefffc91e9ef59cf7
jayvardhan <vardhan.jay@gmail.com>
2021-08-23 14:59:52 +0530

    added category overlay on card template

--- template-parts/orbit/card.php
+++ template-parts/orbit/card.php
@@ -2,9 +2,16 @@
     $post_id = get_the_ID();
     $permalink = get_the_permalink();
     $thumbnail = get_the_post_thumbnail_url($post_id);
+    $categories = get_the_category($post_id);
 ?>
 
-<a class="feat-img" style="background-image:url('<?php _e($thumbnail);?>')" data-id="<?php _e( $post_id );?>" href="<?php _e($permalink);;?>"></a>
+<div class="image-wrapper">
+    <a class="feat-img" style="background-image:url('<?php _e($thumbnail);?>')" data-id="<?php _e( $post_id );?>" href="<?php _e($permalink);;?>">
+       <?php if( is_array($categories) && count($categories) ) : ?>
+            <span class="cat-overlay"><?php _e($categories[0]->name);?></span>
+        <?php endif;?>
+    </a>
+</div>
 
 <div class="content-wrap">
 	<a class="title" data-id="<?php _e( $post_id );?>" href="<?php _e($permalink);?>"><?php the_title();?></a>
