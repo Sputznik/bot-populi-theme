0deaaa9f5d86fd4474c17e16fc013659d097edfa
jayvardhan <vardhan.jay@gmail.com>
2021-09-22 14:58:44 +0530

    refactored orbit templates for SO podcast tab

--- orbit-query/articles-frame-episode.php
+++ orbit-query/articles-frame-episode.php
@@ -21,7 +21,7 @@
                     if( $this->query->have_posts() ) { $this->query->the_post(); ?>
                          <?php $post_type = get_post_type();?>
                         <article class='post-card <?php _e($post_type);?>'>
-                            <?php get_template_part( 'template-parts/orbit/podcast' ); ?>
+                            <?php get_template_part( 'template-parts/orbit/episode' ); ?>
                         </article> <?php
                         $k++ ;
                     } else {
