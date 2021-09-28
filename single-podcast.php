70c8ea059df7e9475bdeedb62150a726fd43d711
psychome004 <anil.stephen66@gmail.com>
2021-09-28 12:44:56 +0530

    Updated shortcode style-attribute value

--- single-podcast.php
+++ single-podcast.php
@@ -1,5 +1,5 @@
-<?php 
-    get_header(); 
+<?php
+    get_header();
     $image = get_the_post_thumbnail_url();
 ?>
 
@@ -13,20 +13,20 @@
             <h1 class="page-title"><?php the_title(); ?></h1>
             <div class="page-title-separator"></div>
             <div class="page-description"><?php the_content(); ?></div>
-            <div class="orbit-posts-wrapper"> <?php 
+            <div class="orbit-posts-wrapper"> <?php
                 $output = do_shortcode('[orbit_query post_type="episode" post_parent__in="'. $post->ID .'" pagination="1" style="episode" posts_per_page="10" order="ASC" button="1" button_title="Back to Podacasts" button_slug="/podcasts"]');
 
                 echo $output; ?>
             </div>
-        <?php endwhile; endif;?>                
+        <?php endwhile; endif;?>
 		</div>
 
         <div class="col-md-12 similar-content">
             <h2 class="title">See Other Podcasts</h2>
-            <?php 
-                $output = do_shortcode('[orbit_query post_type="podcast" post__not_in="'. $post->ID .'" style="episode" posts_per_page="3" ]');
+            <?php
+                $output = do_shortcode('[orbit_query post_type="podcast" post__not_in="'. $post->ID .'" style="podcast" posts_per_page="3" ]');
 
-                echo $output; 
+                echo $output;
             ?>
         </div>
 	</div>
